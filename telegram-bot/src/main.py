949874e1fba31c005cc008c8dec59fdb14567984
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-11-07 01:35:04 -0300

    :sparkles: code: add basic chat

--- telegram-bot/src/main.py
+++ telegram-bot/src/main.py
@@ -1,37 +1,14 @@
-# main code
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
 import logging
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -68,26 +45,34 @@ async def unknow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     answer = """Não entendi sua resposta...
     """
     await update.message.reply_text(answer)
+    
+    
+async def basic_chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handle user response based on message text."""
+    response = update.message.text.lower()  
 
+    responses = {
+        "sim": "Então entre no nosso grupo especial e comece a apostar!!!\n",
+        "não": """Você pode se cadastrar no site e começar hoje mesmo!\n
+                  clique no link abaixo:\n(Restam poucas vagas...)\n<link_do_site>"""
+    }
+
+
+    answer = responses.get(response, "Não entendi sua resposta...")
+    await update.message.reply_text(answer)
 
 def main() -> None:
     """Start the bot."""
-    # Create the Application and pass it your bot's token.
     with open("token.txt") as tokenfile:
         token = tokenfile.read()
     application = Application.builder().token(token).build()
-
-    # on different commands - answer in Telegram
+    
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("Sim", yes_answer))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, unknow))
-
-    # Run the bot until the user presses Ctrl-C
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, basic_chat))
+    
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
